fab1f072a70eca90cc88be38318d1227b0c31e2f
jaemul22 <jaemul22@gmail.com>
2015-06-23 21:30:31 +0700

    Finish log in log out'

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -20,5 +20,6 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
 	  		password:"foobar", password_confirmation:"foobar"}
   	end
   	assert_template 'users/show'
+    assert is_logged_in?
   end
 end
